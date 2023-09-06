c08fc8c9acd735ec443b5f7933ca849126d90789
al6ahiri <109921882+al6ahiri@users.noreply.github.com>
2023-09-06 19:50:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 # write your code here
+person = {
+   "name" : "saud" ,
+   "age" : 18 ,
+   "hoppies" : [ " sleep" ," eat " , "read" ]
+}
+
+print (person ["name"])
+print (person ["age"])
+
+person["age"] = 19
+person["country"] = "afghanistan" 
+print(person)
+print(len(person))
+       
+person["hoppies"].append("runnig")
+def check_hoppies(kw) :
+  if len(kw["hoppies"]) > 3 : 
+    print("wow you are amazing")
+  
+check_hoppies(person)
+
+
+
+
+
+
